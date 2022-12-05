f2f17114f2250807a2982a8ca850917a236829ec
Stan Nieuwmans <snieuwmans@gmail.com>
2022-12-05 14:22:30 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
 from setuptools import setup
+from os import path
+from codecs import open
+
+
+# The directory containing this file
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name='python-InstagramBusinessAPI',
@@ -7,6 +18,8 @@ setup(
     author='Stan Nieuwmans',
     author_email='snieuwmans@gmail.com',
     url='https://github.com/StanNieuwmans/InstagramAPI',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[
         'requests',
         'json',
